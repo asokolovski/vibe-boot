5c110e79a73c2f919b25565504c97ca75ce2c9b7
Alexei Sokolovski <a.sokolovski@icloud.com>
2026-05-12 16:22:03 -0400

    inital spring boot project

---
+++ src/main/java/com/alexeisoki/vibeboot/VibeBootApplication.java
@@ -0,0 +1,13 @@
+package com.alexeisoki.vibeboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VibeBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VibeBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alexeisoki/vibeboot/VibeBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexeisoki.vibeboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VibeBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
